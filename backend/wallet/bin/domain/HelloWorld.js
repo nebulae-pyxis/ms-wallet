801e76811f783de7dc2277060ea06f7d04cdb3e2
Felipe Santa <juan.santa@nebulae.com.co>
2018-10-23 08:12:09 -0500

    customized template

---
+++ backend/wallet/bin/domain/HelloWorld.js
@@ -0,0 +1,113 @@
+"use strict";
+
+const Rx = require("rxjs");
+const HelloWorldDA = require("../data/HelloWorldDA");
+const broker = require("../tools/broker/BrokerFactory")();
+const MATERIALIZED_VIEW_TOPIC = "materialized-view-updates";
+const { take, mergeMap, catchError, map } = require('rxjs/operators');
+const {
+  CustomError,
+  DefaultError
+} = require("../tools/customError");
+
+/**
+ * Singleton instance
+ */
+let instance;
+
+class HelloWorld {
+  constructor() {
+    this.initHelloWorldEventGenerator();
+  }
+
+  /**
+   *  HelloWorld Query, please remove
+   *  this is a queiry form GraphQL
+   */
+  getHelloWorld$(request) {
+    console.log(`request: request`)
+    return HelloWorldDA.getHelloWorld$().pipe(
+      mergeMap(rawResponse => this.buildSuccessResponse$(rawResponse))
+      ,catchError(err => this.errorHandler$(err)) 
+    );
+  }
+
+  /**
+   * Handle HelloWorld Query, please remove
+   * This in an Event HAndler for Event- events
+   */
+  handleHelloWorld$(evt) {
+    return Rx.of('Some process for HelloWorld event');
+  }
+
+
+  initHelloWorldEventGenerator(){
+    Rx.interval(1000).pipe(
+      take(120)
+      ,mergeMap(id =>  HelloWorldDA.getHelloWorld$())    
+      ,mergeMap(evt => {
+        return broker.send$(MATERIALIZED_VIEW_TOPIC, 'businessWalletHelloWorldEvent',evt);
+      })
+    )
+    .subscribe(
+      (evt) => console.log('emi-gateway GraphQL sample event sent, please remove'),
+      (err) => { 
+        console.log(err);
+        console.error('emi-gateway GraphQL sample event sent ERROR, please remove')
+      },
+      () => console.log('emi-gateway GraphQL sample event sending STOPPED, please remove'),
+    );
+  }
+
+
+
+
+  //#region  mappers for API responses
+  errorHandler$(err) {
+    return Rx.of(err).pipe(
+      map(err => {
+        const exception = { data: null, result: {} };
+        const isCustomError = err instanceof CustomError;
+        if(!isCustomError){
+          err = new DefaultError(err)
+        }
+        exception.result = {
+            code: err.code,
+            error: {...err.getContent()}
+          }
+        return exception;
+      }  
+    )
+    );
+  }
+
+  
+  buildSuccessResponse$(rawRespponse) {
+    return Rx.of(rawRespponse)
+      .pipe(
+        map(resp => {
+          return {
+            data: resp,
+            result: {
+              code: 200
+            }
+          }
+        })
+      );
+  }
+
+  //#endregion
+
+
+}
+
+/**
+ * @returns {HelloWorld}
+ */
+module.exports = () => {
+  if (!instance) {
+    instance = new HelloWorld();
+    console.log(`${instance.constructor.name} Singleton created`);
+  }
+  return instance;
+};
---
+++ backend/wallet/bin/services/emi-gateway/GraphQlService.js
@@ -0,0 +1,179 @@
+"use strict";
+
+const helloWorld = require("../../domain/HelloWorld")();
+const broker = require("../../tools/broker/BrokerFactory")();
+const Rx = require("rxjs");
+const jsonwebtoken = require("jsonwebtoken");
+const { map, mergeMap, catchError, tap } = require('rxjs/operators');
+const jwtPublicKey = process.env.JWT_PUBLIC_KEY.replace(/\\n/g, "\n");
+
+
+let instance;
+
+class GraphQlService {
+
+
+  constructor() {
+    this.functionMap = this.generateFunctionMap();
+    this.subscriptions = [];
+  }
+
+  /**
+   * Starts GraphQL actions listener
+   */
+  start$() {
+      //default on error handler
+      const onErrorHandler = (error) => {
+        console.error("Error handling  GraphQl incoming event", error);
+        process.exit(1);
+      };
+  
+      //default onComplete handler
+      const onCompleteHandler = () => {
+        () => console.log("GraphQlService incoming event subscription completed");
+      };
+    return Rx.from(this.getSubscriptionDescriptors()).pipe(
+      map(aggregateEvent => ({ ...aggregateEvent, onErrorHandler, onCompleteHandler }))
+      ,map(params => this.subscribeEventHandler(params))
+    )
+  }
+
+  /**
+   * build a Broker listener to handle GraphQL requests procesor
+   * @param {*} descriptor 
+   */
+  subscribeEventHandler({
+    aggregateType,
+    messageType,
+    onErrorHandler,
+    onCompleteHandler
+  }) {
+    const handler = this.functionMap[messageType];
+    const subscription = broker
+      .getMessageListener$([aggregateType], [messageType]).pipe(
+        mergeMap(message => this.verifyRequest$(message))
+        ,mergeMap(request => ( request.failedValidations.length > 0)
+          ? Rx.of(request.errorResponse)
+          : Rx.of(request).pipe(
+              //ROUTE MESSAGE TO RESOLVER
+              mergeMap(({ authToken, message }) =>
+              handler.fn
+                .call(handler.obj, message.data, authToken).pipe(
+                  map(response => ({ response, correlationId: message.id, replyTo: message.attributes.replyTo }))
+                )
+            )
+          )
+        )    
+        ,mergeMap(msg => this.sendResponseBack$(msg))
+      )
+      .subscribe(
+        msg => { /* console.log(`GraphQlService: ${messageType} process: ${msg}`); */ },
+        onErrorHandler,
+        onCompleteHandler
+      );
+    this.subscriptions.push({
+      aggregateType,
+      messageType,
+      handlerName: handler.fn.name,
+      subscription
+    });
+    return {
+      aggregateType,
+      messageType,
+      handlerName: `${handler.obj.name}.${handler.fn.name}`
+    };
+  }
+
+    /**
+   * Verify the message if the request is valid.
+   * @param {any} request request message
+   * @returns { Rx.Observable< []{request: any, failedValidations: [] }>}  Observable object that containg the original request and the failed validations
+   */
+  verifyRequest$(request) {
+    return Rx.of(request).pipe(
+      //decode and verify the jwt token
+      mergeMap(message =>
+        Rx.of(message).pipe(
+          map(message => ({ authToken: jsonwebtoken.verify(message.data.jwt, jwtPublicKey), message, failedValidations: [] }))
+          ,catchError(err =>
+            EventSourcingMonitor.errorHandler$(err).pipe(
+              map(response => ({
+                errorResponse: { response, correlationId: message.id, replyTo: message.attributes.replyTo },
+                failedValidations: ['JWT']
+              }
+              ))
+            )
+          )
+        )
+      )
+    )
+  }
+
+ /**
+  * 
+  * @param {any} msg Object with data necessary  to send response
+  */
+ sendResponseBack$(msg) {
+   return Rx.of(msg).pipe(mergeMap(
+    ({ response, correlationId, replyTo }) =>
+      replyTo
+        ? broker.send$(replyTo, "emi-gateway.graphql.Query.response", response, {
+            correlationId
+          })
+        : Rx.of(undefined)
+  ));
+}
+
+  stop$() {
+    Rx.from(this.subscriptions).pipe(
+      map(subscription => {
+        subscription.subscription.unsubscribe();
+        return `Unsubscribed: aggregateType=${aggregateType}, eventType=${eventType}, handlerName=${handlerName}`;
+      })
+    );
+  }
+
+  ////////////////////////////////////////////////////////////////////////////////////////
+  /////////////////// CONFIG SECTION, ASSOC EVENTS AND PROCESSORS BELOW  /////////////////
+  ////////////////////////////////////////////////////////////////////////////////////////
+
+
+  /**
+   * returns an array of broker subscriptions for listening to GraphQL requests
+   */
+  getSubscriptionDescriptors() {
+    console.log("GraphQl Service starting ...");
+    return [
+      //Sample incoming request, please remove
+      {
+        aggregateType: "HelloWorld",
+        messageType: "emi-gateway.graphql.query.getHelloWorldFromwallet"
+      }     
+    ];
+  }
+
+
+  /**
+   * returns a map that assocs GraphQL request with its processor
+   */
+  generateFunctionMap() {    
+    return {
+      //Sample incoming request, please remove
+      "emi-gateway.graphql.query.getHelloWorldFromwallet": {
+        fn: helloWorld.getHelloWorld$,
+        obj: helloWorld
+      },      
+    };
+  }
+}
+
+/**
+ * @returns {GraphQlService}
+ */
+module.exports = () => {
+  if (!instance) {
+    instance = new GraphQlService();
+    console.log(`${instance.constructor.name} Singleton created`);
+  }
+  return instance;
+};
