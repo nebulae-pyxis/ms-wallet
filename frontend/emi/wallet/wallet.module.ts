8b953447a634129067142389886beb90a768c655
Felipe Santa <juan.santa@nebulae.com.co>
2018-11-14 18:10:48 -0500

    pipes added to the declaration array at ngModule

--- frontend/emi/wallet/wallet.module.ts
+++ frontend/emi/wallet/wallet.module.ts
@@ -43,7 +43,8 @@ const routes: Routes = [
     TransactionHistoryDetailComponent,
     ManualPocketAdjustmentComponent,
     CurrencyFormatterDirective,
-    DialogComponent
+    DialogComponent,
+    CurrencyPipe
   ],
   entryComponents: [DialogComponent],
   providers: [ WalletService, DatePipe, ManualPocketAdjustmentService, TransactionHistoryService, TransactionHistoryDetailService, CurrencyPipe]
